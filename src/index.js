44813e6bc851d253e79c011c142aaac17c308b52
hsp <hsp_email@163.com>
2020-06-30 17:58:58 +0800

    删除了LJ代码

--- src/index.js
+++ src/index.js
@@ -6,14 +6,6 @@ import {
 } from 'react-redux'
 import store from './redux/store'
 
-// import storageUtils from './utils/storageUtils'
-// import memoryUtils from './utils/memoryUtils'
-// // 如果 local 中保存了 user, 将 user 保存到内存中
-// const user = storageUtils.getUser()
-// if (user && user._id) {
-//     memoryUtils.user = user
-// }
-
 // 将App组件标签渲染到index页面的div上
 ReactDOM.render((
     <Provider store={store}>
